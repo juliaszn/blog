d6c756d55a9d03b0da0214275b3398a995e401b9
juliaszn <yuliaaaas04@gmail.com>
2025-03-27 16:16:11 +0400

    Implemented routes and controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $articles = Article::get();
+        return view('article.index', compact('articles'));
+    }
+
+    public function about() {
+        return 'about';
+    }
+}
